3eb18bce5a66d4da0356c8f62e17bc46eb014253
oliverwalsh7 <oliverwalsh7@gmail.com>
2020-08-03 22:24:44 -0400

    practice makes perfect

--- tfprac.py
+++ tfprac.py
@@ -2,5 +2,11 @@
 # oliver walsh august 2020
 
 import tensorflow as tf
-
 print(tf.version)
+
+# tf.reshape(x, -1): -1 will calculate tensor size
+# types of tensors: variables (mutable), constants, placeholders, sparsetensors
+# tensor.eval() finds tensor vals
+
+t = tf.zeros(5, 5, 5, 5)
+print(t)
