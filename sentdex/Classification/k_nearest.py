8eb98eefb4ae2cdaa7b7628da7fdba5a02214b6e
oliverwalsh7 <oliverwalsh7@gmail.com>
2020-12-27 23:47:59 -0500

    mac?

--- sentdex/Classification/k_nearest.py
+++ sentdex/Classification/k_nearest.py
@@ -17,7 +17,7 @@ accuracy = clf.score(X_test, y_test)
 print(accuracy)
 
 example_measures = np.array([4, 2, 1, 1, 1, 2, 3, 2, 1])
-example_measures = example_measures.reshape(1, -1)
+example_measures = example_measures.reshape(len(example_measures), -1)
 
 prediction = clf.predict(example_measures)
 print(prediction)
