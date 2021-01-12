b678de2fd0c46d45bfa5c5e32a6e889114cafdf7
oliverwalsh7 <oliverwalsh7@gmail.com>
2021-01-12 11:42:50 -0500

    tut 26

--- sentdex/SVM/svm_from_scratch.py
+++ sentdex/SVM/svm_from_scratch.py
@@ -13,12 +13,49 @@ class Support_Vector_Machine:
     
     # train
     def fit(self, data):
+        self.data = data
+        # {||w||: [w, b]}
+        opt_dict = {}
+
+        transforms = [[1, 1], [-1, 1], [-1, -1], [1, -1]]
+
+        all_data = []
+        for yi in self.data:
+            for featureset in self.data[yi]:
+                for feature in featureset:
+                    all_data.append(feature)
+        
+        self.max_feature_value = max(all_data)
+        self.min_feature_value = min(all_data)
+
+        all_data = None
+
+        step_sizes = [self.max_feature_value * 0.1,
+                      self.max_feature_value * 0.01,
+                      # point of expense:
+                      self.max_feature_value * 0.001] # step size (.1 is big step, 0.01 is less, etc)
+
+        # extremely expensive
+        b_range_multiple = 5
+        # 
+        b_multiple = 5
+
+        latest_optimum = self.max_feature_value * 10 # corner-cutting
+
+        # begin stepping
+        for step in step_sizes:
+            w = np.array([latest_optimum, latest_optimum])
+            
+            # we can do this because convex
+            optimized = False
+            while not optimized:
+                pass
+
         pass
 
     def predict(self, features):
         # sign(x.w + b)
         classification = np.sign(np.dot(np.array(features), self.w) + self.b)
-
         return classification
 
 data_dict = {-1: np.array([[1,7], [2,8], [3,8]]), 
